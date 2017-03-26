c413c79155a76aebb54d6d7b1ebccf654a1c749e
Vestelth <vestelth@gmail.com>
2017-03-27 01:36:43 +0200

    gotta help the ghost decide the consistent direction

--- js/ghost-collision.js
+++ js/ghost-collision.js
@@ -43,23 +43,6 @@ let ghostMovement = () => {
     down  : false
   }
 
-  let checkRight = (gX, gY, blX, blY) => {
-    if (gX + ghostSize + 5  > blX && gX < blX &&
-        gY + ghostSize > blY && gY < blY + blockSize
-        || gX + ghostSize > 518)
-    {
-        obstacle.right = true;
-        console.log(obstacle.right);
-        return true;
-    } else {
-        console.log(obstacle.right);
-        return false;
-    }
-  }
-
-
-
-
   //making an array with all obstacle positions
   $('.block').each( function(index) {
     x = $(this).position().left ;
@@ -70,8 +53,7 @@ let ghostMovement = () => {
   let checkCollRight = (ghost) => {
       let gX = ghost.position().left;
       let gY = ghost.position().top;
-      let obstacle = { right  : false };
-      //console.log(gX + ghostSize);
+
       for ( let i = 0; i < obj2Pos.length; i++ ){
         if (gX + ghostSize + 4 >= obj2Pos[i][0] &&
             gX <= obj2Pos[i][0] &&
@@ -79,34 +61,31 @@ let ghostMovement = () => {
             gY - 4 < obj2Pos[i][1] + blockSize ||
             gX + ghostSize >= 516)
         {
-          obstacle.right = true;
           return true;
         }
       }
   }
 
-  let checkCollisionLeft = () => {
+  let checkCollLeft = (ghost) => {
       let gX = ghost.position().left;
       let gY = ghost.position().top;
-      let obstacle = { left  : false };
 
       for ( let i = 0; i < obj2Pos.length; i++ ){
-        if (gX - 4 <= objPos[i][0] + blockSize &&
+        if (gX - 4 <= obj2Pos[i][0] + blockSize &&
             gX + ghostSize >= obj2Pos[i][0] &&
             gY + ghostSize + 4 > obj2Pos[i][1] &&
             gY - 4 < obj2Pos[i][1] + blockSize
             || gX <= 4)
         {
-          obstacle.left = true;
           return true;
         }
       }
   }
 
-  let checkCollisionUp = () => {
+  let checkCollUp = (ghost) => {
       let gX = ghost.position().left;
       let gY = ghost.position().top;
-      let obstacle = { up  : false }
+
       for ( let i = 0; i < obj2Pos.length; i++ ){
         if (gY - 4  <= obj2Pos[i][1] + blockSize &&
             gY + ghostSize > obj2Pos[i][1] &&
@@ -114,24 +93,22 @@ let ghostMovement = () => {
             gX - 4 < obj2Pos[i][0] + blockSize
             || gY <= 4)
         {
-          obstacle.up = true;
           return true;
         }
       }
   }
 
-  let checkCollisionDown = () => {
+  let checkCollDown = (ghost) => {
       let gX = ghost.position().left;
       let gY = ghost.position().top;
-      let obstacle = { down  : false }
+
       for ( let i = 0; i < obj2Pos.length; i++ ){
-        if (gY + ghostSize >= obj2Pos[i][1] &&
+        if (gY + ghostSize + 4 >= obj2Pos[i][1] &&
             gY < obj2Pos[i][1] &&
-            gX + ghostSize - 2 > obj2Pos[i][0] &&
-            gX < obj2Pos[i][0] + objSize
+            gX + ghostSize > obj2Pos[i][0] &&
+            gX < obj2Pos[i][0] + blockSize
             || gY + ghostSize >= 518)
         {
-          obstacle.down = true;
           return true;
         }
       }
@@ -146,19 +123,70 @@ let ghostMovement = () => {
     ghost.css({'top' : gY });
   }
 
-  let time = setInterval(function () {
+  // let time = setInterval(function () {
     $('.ghost').each( function(index) {
       let thisGhost = $(this);
       let ghostX = $(this).position().left;
       let ghostY = $(this).position().top;
+      let possibleDir = ['right','left','up','down'];
 
-        if (checkCollRight(thisGhost)) {
-          moveGhost(0, -1, thisGhost);
+      if (checkCollRight(thisGhost)) {
+        console.log('collision right detected');
+        for (var i = 0; i < possibleDir.length; i++) {
+          if (possibleDir[i] === 'right'){
+            possibleDir.splice(i, 1);
+          }
         }
+      }
+      if (checkCollLeft(thisGhost)) {
+        console.log('collision left detected');
+        for (var i = 0; i < possibleDir.length; i++) {
+          if (possibleDir[i] === 'left'){
+            possibleDir.splice(i, 1);
+          }
+        }
+      }
+      if (checkCollUp(thisGhost)) {
+        console.log('collision up detected');
+        for (var i = 0; i < possibleDir.length; i++) {
+          if (possibleDir[i] === 'up'){
+            possibleDir.splice(i, 1);
+          }
+        }
+      }
+      if (checkCollDown(thisGhost)) {
+        console.log('collision down detected');
+        for (var i = 0; i < possibleDir.length; i++) {
+          if (possibleDir[i] === 'down'){
+            possibleDir.splice(i, 1);
+          }
+        }
+      }
 
+      let newDir =  Math.round( Math.random() * (possibleDir.length - 1) );
+
+      console.log(possibleDir[newDir]);
+
+      switch(possibleDir[newDir]) {
+        case 'left':
+          console.log('case left');
+          moveGhost(-1 , 0, thisGhost);
+          break;
+        case 'right':
+          console.log('case right');
+          moveGhost(1, 0, thisGhost);
+          break;
+        case 'up':
+          console.log('case up');
+          moveGhost(0, -1, thisGhost);
+          break;
+        case 'down':
+          console.log('case down');
+          moveGhost(0, 1, thisGhost);
+      }
 
     });
 
-  }, 50);
+  // }, 200);
 
 }
